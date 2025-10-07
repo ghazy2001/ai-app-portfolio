6a3209419a191dc99b24605b52667530f644999a
GhaZy <mahmoudghazy2001@gmail.com>
2025-10-07 14:47:55 +0300

    Fix import paths in components index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-export { default as Article } from "./Article/Article";
-export { default as Brand } from "./Brand/Brand";
-export { default as CTA } from "./CTA/CTA";
-export { default as Feature } from "./Feature/Feature";
-export { default as Navbar } from "./Navbar/Navbar";
+export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta/Cta";
+export { default as Feature } from "./feature/Feature";
+export { default as Navbar } from "./navbar/Navbar";
